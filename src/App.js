deec8a57901f8abcd8cca4d8febb4eb51429e899
Issac Abraham <issaca32@gmail.com>
2021-09-15 16:13:28 +0100

    authorizes login to retrieve token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import Login from './Login';
+import {useEffect} from "react";
+import {getTokenFromURL} from "./spotify";
+import {useState} from "react";
+import Player from "./Player";
+import SpotifyWebAPI from 'spotify-web-api-js';
 
+const SPOTIFY = new SpotifyWebAPI();
 
 function App() {
+    const [token, setToken] = useState(null)
+    useEffect(()=>{
+        const hash = getTokenFromURL();
+        window.location.hash = "";
+        const _token = hash.access_token;
+        if (_token){
+            setToken(_token);
+            SPOTIFY.setAccessToken(_token);
+            SPOTIFY.getMe().then(r => console.log(r));
+        }
+
+    }, [])
   return (
     <div className="app">
-      <Login />
+        {token ? (<Player/>): <Login />}
     </div>
   );
 }
